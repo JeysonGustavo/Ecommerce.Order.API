c168eecc2d423d7414931692fdcfccc75a17c647
jeysongustavo <jeyson.gomesr@stgen.com>
2022-11-10 18:05:57 -0300

    general improvements

--- Ecommerce.Order.API.Core/Models/Response/ProductMessageResponseModel.cs
+++ Ecommerce.Order.API.Core/Models/Response/ProductMessageResponseModel.cs
@@ -7,15 +7,17 @@
         public int ProductId { get; set; }
         public int Units { get; set; }
         public bool IsSuccess { get; set; }
+        public string DatabaseToDoOperation { get; set; }
         #endregion
 
         #region Constructor
-        public ProductMessageResponseModel(int orderId, int productId, int units, bool isSuccess)
+        public ProductMessageResponseModel(int orderId, int productId, int units, bool isSuccess, string databaseToDoOperation)
         {
             OrderId = orderId;
             ProductId = productId;
             Units = units;
             IsSuccess = isSuccess;
+            DatabaseToDoOperation = databaseToDoOperation;
         }
         #endregion
     }
